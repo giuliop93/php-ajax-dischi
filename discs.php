e9df9bd70e55ba7bf14c4aabd62e054f8490f4a1
Giulio_Pa <giuliop93@hotmail.it>
2021-06-03 18:31:27 +0200

    wip

---
+++ discs.php
@@ -0,0 +1,62 @@
+<?php
+
+$arrayDiscs = [
+    [
+        'title' => "Viva la vida or Death and All His Friends",
+        'author' => "Coldplay",
+        'album' => "",
+        'year' => "2008"
+    ],
+    [
+        'title' => "Lost on You",
+        'author' => "Laura Pergolizzi",
+        'album' => "",
+        'year' => "2016"
+    ],    [
+        'title' => "In Time: The Best of R.E.M.",
+        'author' => "R.E.M.",
+        'album' => "",
+        'year' => "2003"
+    ],    [
+        'title' => "Tutto L'amore Che Conta Davvero",
+        'author' => "Mango",
+        'album' => "",
+        'year' => "2019"
+    ],    [
+        'title' => "Greatest Hits",
+        'author' => "Robbie Williams",
+        'album' => "",
+        'year' => "2004"
+    ],    [
+        'title' => "Vasco Modena Park",
+        'author' => "Vasco Rossi",
+        'album' => "https://images-na.ssl-images-amazon.com/images/I/61MYqNeYw5L._AC_SX425_.jpg",
+        'year' => "2017"
+    ],    [
+        'title' => "Bohemian Rhapsody: The Original Soundtrack",
+        'author' => "Queen",
+        'album' => "",
+        'year' => "2018"
+    ],    [
+        'title' => "Se l'amore è amore",
+        'author' => "Antonello Venditti",
+        'album' => "",
+        'year' => "2000"
+    ],    [
+        'title' => "The Best of Depeche Mode",
+        'author' => "Depeche Mode",
+        'album' => "",
+        'year' => "2006"
+    ],    [
+        'title' => "TuttoMax",
+        'author' => "Max Pezzali",
+        'album' => "",
+        'year' => "2005"
+    ],    [
+        'title' => "The Platinum Collection",
+        'author' => "Matia Bazar",
+        'album' => "",
+        'year' => "2007"
+    ],
+    
+]
